2e9906ea9f326cf2d83f2ecaa550376998576dff
Raul11jg <raul11jg@gmail.com>
2025-11-12 18:44:01 +0100

    add hero section strapi

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,3 +1,39 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ComponentLink extends Struct.ComponentSchema {
+  collectionName: 'components_component_links';
+  info: {
+    displayName: 'Link';
+    icon: 'link';
+  };
+  attributes: {
+    isExternal: Schema.Attribute.Boolean;
+    name: Schema.Attribute.String;
+    url: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'#'>;
+  };
+}
+
+export interface LayoutHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    icon: 'house';
+  };
+  attributes: {
+    headerLink: Schema.Attribute.Component<'component.link', false>;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    subHeader: Schema.Attribute.Text;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'component.link': ComponentLink;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
+}
